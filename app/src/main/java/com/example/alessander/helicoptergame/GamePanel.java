95ef5515128e1d049228bb854d16d0bb2f383ff8
alessanderleite <alessander.logistica@gmail.com>
2018-11-18 17:22:46 -0200

    creating a new class called MainThread

--- app/src/main/java/com/example/alessander/helicoptergame/GamePanel.java
+++ app/src/main/java/com/example/alessander/helicoptergame/GamePanel.java
@@ -6,12 +6,17 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+    private MainThread thread;
 
     public GamePanel(Context context) {
         super(context);
 
         //Add the callback to the SurfaceHolder to intercept events
         getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+
+        setFocusable(true);
     }
 
     @Override
@@ -21,16 +26,32 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                retry = false;
+            }
+        }
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
 
+        //safely start the game loop
+        thread.setRunning(true);
+        thread.start();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         return super.onTouchEvent(event);
+    }
+
+    public void update() {
+
     }
 }
