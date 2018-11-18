a765948caf716cc80d0564daed1f9bf9345dc7a2
alessanderleite <alessander.logistica@gmail.com>
2018-11-18 20:10:56 -0200

    background class

---
+++ app/src/main/java/com/example/alessander/helicoptergame/Background.java
@@ -0,0 +1,19 @@
+package com.example.alessander.helicoptergame;
+
+import android.graphics.Bitmap;
+
+public class Background {
+
+    private Bitmap image;
+    private int x, y;
+
+    public Background(Bitmap res) {
+        image = res;
+    }
+    public void update() {
+
+    }
+    public void draw() {
+        
+    }
+}
